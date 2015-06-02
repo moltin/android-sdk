3e44e72fc0a7de2859dd27762dcf05a578e5f966
Marko <marek1985@gmail.com>
2015-06-02 20:54:16 +0200

    Tax endpoint bugfix

--- src/main/java/moltin/android_sdk/endpoints/Tax.java
+++ src/main/java/moltin/android_sdk/endpoints/Tax.java
@@ -3,8 +3,7 @@ package moltin.android_sdk.endpoints;
 import android.os.Handler;
 import android.os.Message;
 
-import org.json.JSONObject;
-
+import moltin.android_sdk.models.Pagination;
 import moltin.android_sdk.utilities.Constants;
 import moltin.android_sdk.utilities.Preferences;
 
@@ -55,7 +54,7 @@ public class Tax extends TaxAbstract {
     }
 
     @Override
-    public void find(final JSONObject terms, final Handler.Callback callback) throws Exception {
+    public void find(final moltin.android_sdk.models.Tax terms, final Handler.Callback callback) throws Exception {
         if(preferences.isExpired())
         {
             Authenticate authenticate = new Authenticate(preferences);
@@ -93,7 +92,7 @@ public class Tax extends TaxAbstract {
     }
 
     @Override
-    public void list(final JSONObject terms, final Handler.Callback callback) throws Exception {
+    public void list(final Pagination terms, final Handler.Callback callback) throws Exception {
         if(preferences.isExpired())
         {
             Authenticate authenticate = new Authenticate(preferences);
